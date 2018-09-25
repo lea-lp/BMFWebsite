faf9e38058da9390e3e554f423170c026a26a844
Lea Leipp <lea.leipp@gmail.com>
2018-09-25 13:43:30 +0200

    maj js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert('test yo!')
+function redirectionX(){
+    if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')){
+        location.href =  'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+};
+setTimeout(redirectionX, 10000);
