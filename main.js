43f0c119f2d1613236d020e851fa982b85f41b3c
Lea Leipp <lea.leipp@gmail.com>
2018-09-25 13:17:17 +0200

    MAJ

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-( function() {
-  ( "#dialog" ).dialog({
-    autoOpen: false,
-    show: {
-      effect: "blind",
-      duration: 1000
-    },
-    hide: {
-      effect: "explode",
-      duration: 1000
-    }
-  });
-
-  ( "#opener" ).on( "click", function() {
-    ( "#dialog" ).dialog( "open" );
-  });
-} );
+alert('test yo!')
