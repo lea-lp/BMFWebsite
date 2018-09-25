577bde98f3d1f094eb014d98a94916505607a235
lea-lp <43336547+lea-lp@users.noreply.github.com>
2018-09-25 13:36:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert('test yo!')
+function redirectionX(){
+    if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')){
+        location.href =  'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+};
+setTimeout(redirectionX, 10000);
+
